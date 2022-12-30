ad385e0d79ebf9de9d45ecdb7989ab68d573cd50
Gajjya18 <113501096+Gajjya18@users.noreply.github.com>
2022-12-30 22:43:42 +0530

    yes

--- BACKEND/loan_for_company_interior/loan_for_company_interior/settings.py
+++ BACKEND/loan_for_company_interior/loan_for_company_interior/settings.py
@@ -176,13 +176,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # EMAIL_PORT = env('EMAIL_PORT')
 
-DEFAULT_FROM_EMAIL = 'gajananpathak18@gmail.com'
+DEFAULT_FROM_EMAIL = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gajananpathak18@gmail.com'
-EMAIL_HOST_PASSWORD = 'vxkwjqwnotprhkpz'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 LOGGING = {
     'version' : 1,
